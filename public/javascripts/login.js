d69d28243e4b14e78b62d0cc17395617b0d16183
Shadab Mahtabi <shadabmanpuri@gmail.com>
2024-03-25 01:11:14 +0530

    Responsive Login_page and fixes dark mode of sidebar

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,19 +1,48 @@
-const sign_up_btn = document.querySelector('#signUp');
-const hider = document.querySelector('#hider');
+const sign_up_btn = document.querySelector("#signUp");
+const hider = document.querySelector("#hider");
+const loginForm = document.querySelector(".loginForm");
+const registerForm = document.querySelector(".registerForm");
 
 var flag = 0;
-sign_up_btn.addEventListener('click', () => {
-  if (flag === 0) {
-    hider.style.transform = 'translate(-75%)'
-    document.querySelector('#heading').innerHTML = "One of Us ?"
-    document.querySelector('#text').innerHTML = "Click the Sign In button to login yourself"
-    sign_up_btn.innerHTML = "Sign In"
-    flag = 1;
+sign_up_btn.addEventListener("click", () => {
+  if (window.innerWidth > 500) {
+    if (flag === 0) {
+      hider.style.transform = "translate(-75%)";
+      document.querySelector("#heading").innerHTML = "One of Us ?";
+      document.querySelector("#text").innerHTML =
+        "Click the Sign In button to login yourself";
+      sign_up_btn.innerHTML = "Sign In";
+      flag = 1;
+    } else {
+      hider.style.transform = "translate(0)";
+      document.querySelector("#heading").innerHTML = "New to Majma ?";
+      document.querySelector("#text").innerHTML =
+        "Click the Sign Up button to register yourself";
+      sign_up_btn.innerHTML = "Sign Up";
+      flag = 0;
+    }
   } else {
-    hider.style.transform = 'translate(0)'
-    document.querySelector('#heading').innerHTML = "New to Majma ?"
-    document.querySelector('#text').innerHTML = "Click the Sign Up button to register yourself"
-    sign_up_btn.innerHTML = "Sign Up"
-    flag = 0;
+    console.log(window.innerWidth)
+    if (flag === 0) {
+      hider.style.transform = "translate(0, -110%)";
+      hider.style.alignItems = "flex-end";
+      document.querySelector("#heading").innerHTML = "One of Us ?";
+      document.querySelector("#text").innerHTML =
+      "Click the Sign In button to login yourself";
+      sign_up_btn.innerHTML = "Sign In";
+      loginForm.style.opacity = "0";
+      registerForm.style.opacity = "1";
+      flag = 1;
+    } else {
+      hider.style.transform = "translate(0)";
+      hider.style.alignItems = "initial";
+      document.querySelector("#heading").innerHTML = "New to Majma ?";
+      document.querySelector("#text").innerHTML =
+      "Click the Sign Up button to register yourself";
+      sign_up_btn.innerHTML = "Sign Up";
+      registerForm.style.opacity = "0";
+      loginForm.style.opacity = "1";
+      flag = 0;
+    }
   }
-})
+});
